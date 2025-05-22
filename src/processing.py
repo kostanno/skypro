7bef803ad9d0658bdd9cf87a1154cf9365be4c87
Kostya Igonin <Igoninqw@gmail.com>
2025-05-22 10:59:31 +0500

    done homework

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,12 @@
-def filter_by_state(dict,state="EXECUTED"):
+def filter_by_state(dict, state="EXECUTED"):
     """сортировка словарей"""
     new_dict = []
-    for value in dict :
+    for value in dict:
         if value.get("state") == state:
             new_dict.append(value)
     return new_dict
 
 
-def sort_by_date(dict, sta = "True"):
+def sort_by_date(dict, sta="True"):
     """сортировка словарей по дате"""
     return sorted(dict, key=lambda item: item['date'], reverse=True)
