ae6d0834579aa4d2456757565fea54c15824a553
Kostya Igonin <Igoninqw@gmail.com>
2025-05-22 10:49:52 +0500

    develop

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 def filter_by_state(dict,state="EXECUTED"):
+    """сортировка словарей"""
     new_dict = []
     for value in dict :
         if value.get("state") == state:
@@ -6,10 +7,6 @@ def filter_by_state(dict,state="EXECUTED"):
     return new_dict
 
 
-def sort_by_date(dict, sta = True):
+def sort_by_date(dict, sta = "True"):
+    """сортировка словарей по дате"""
     return sorted(dict, key=lambda item: item['date'], reverse=True)
-
-
-
-
-
