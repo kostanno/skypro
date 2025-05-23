cb282fb4654e8641ae88133194a6ef21913501ce
Kostya Igonin <Igoninqw@gmail.com>
2025-05-23 12:11:33 +0500

    check processing

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,12 @@
-def filter_by_state(dict, state="EXECUTED"):
-    """сортировка словарей"""
-    new_dict = []
-    for value in dict:
+def filter_by_state(dicts: dict, state="EXECUTED") -> list:
+    """сортировка словарей по ключу state="EXECUTED" """
+    sort_dict = []
+    for value in dicts:
         if value.get("state") == state:
-            new_dict.append(value)
-    return new_dict
+            sort_dict.append(value)
+    return sort_dict
 
 
-def sort_by_date(dict, sta="True"):
-    """сортировка словарей по дате"""
-    return sorted(dict, key=lambda item: item['date'], reverse=True)
+def sort_by_date(dicta: dict, sta="True") -> list:
+    """сортировка словарей по дате(по умолчанию-убывание)"""
+    return sorted(dicta, key=lambda item: item["date"], reverse=sta)
